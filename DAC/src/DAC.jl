9405441849e0dbe80781a78341ee1c9ed249a0dc
Agandaur1 <okaresmithn@gmail.com>
2023-03-28 02:29:06 +0000

    updated DAC.jl

--- DAC/src/DAC.jl
+++ DAC/src/DAC.jl
@@ -2,13 +2,16 @@ module DAC
 
 using LinearAlgebra
 using Random
+using PyCall
 
 include("MyLib.jl")
 
 include("Atoms.jl")
+include("Workhorse.jl")
 include("CNA.jl")
 include("FIRE.jl")
 include("LJ.jl")
+include("LJ_ASAP3.jl")
 include("MetC.jl")
 include("generateRandomSeed.jl")
 include("BasinHopping.jl")
