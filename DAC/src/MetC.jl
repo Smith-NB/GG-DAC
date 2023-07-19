13cbe6903b29908ab72fd8339685971b5b0923f2
Agandaur1 <okaresmithn@gmail.com>
2023-07-19 14:17:05 +1200

    reorganised MetC.jl and added HISTOMetC

--- DAC/src/MetC.jl
+++ DAC/src/MetC.jl
@@ -1,26 +1,15 @@
 abstract type MetC end
 
+
+#=============================================================================#
+#=================================EnergyMetC==================================#
+#=============================================================================#
+
 struct EnergyMetC <: MetC
 	kT::Float64
 	io::Tuple{IO, Channel}
 end
 
-
-struct EnergyWithReferenceRestrictionMetC <: MetC
-	kT::Float64
-	refCNA::CNAProfile
-	sigmaCut::Float64
-	io::Tuple{IO, Channel}
-end
-
-struct EnergyAndStructureMetC <: MetC
-	cE::Float64
-	cSCM::Float64
-	eFunction::Function
-	simFunction::Function
-	autoAcceptDownhillMove::Bool
-	io::Tuple{IO, Channel}
-end
 
 """
 	getAcceptanceBoolean(MetC::EnergyMetC, oldCluster::Cluster, newCluster::Cluster)
@@ -47,6 +36,18 @@ function getAcceptanceBoolean(MetC::EnergyMetC, oldCluster::Cluster, newCluster:
 	accept = probability > rand()
 	return accept
 
+end
+
+
+#=============================================================================#
+#============================getAcceptanceBoolean=============================#
+#=============================================================================#
+
+struct EnergyWithReferenceRestrictionMetC <: MetC
+	kT::Float64
+	refCNA::CNAProfile
+	sigmaCut::Float64
+	io::Tuple{IO, Channel}
 end
 
 """
@@ -85,6 +86,19 @@ function getAcceptanceBoolean(MetC::EnergyWithReferenceRestrictionMetC, oldClust
 end
 
 
+#=============================================================================#
+#===========================EnergyAndStructureMetC============================#
+#=============================================================================#
+
+struct EnergyAndStructureMetC <: MetC
+	cE::Float64
+	cSCM::Float64
+	eFunction::Function
+	simFunction::Function
+	autoAcceptDownhillMove::Bool
+	io::Tuple{IO, Channel}
+end
+
 """
 	getAcceptanceBoolean(MetC::EnergyAndStructureMetC, oldCluster::Cluster, newCluster::Cluster)
 
@@ -109,3 +123,83 @@ function getAcceptanceBoolean(MetC::EnergyAndStructureMetC, oldCluster::Cluster,
 	return accept
 
 end	
+
+
+#=============================================================================#
+#=================================HISTOMetC===================================#
+#=============================================================================#
+
+mutable struct HISTOMetC <: MetC
+	kT::Float64
+	w::Float64
+	delta::Float64
+	resetPeriod::Float64
+	refCNA::Vector{Pair{Tuple{UInt8, UInt8, UInt8}, UInt16}}
+	refID::Int64
+	clusterVector::ClusterVector
+	waitTime::Int64
+	timeElapsed::Int64
+	hist::Vector{Int64}
+	io::Tuple{IO, Channel}
+end
+
+function HISTOMetC(kT::Float64, w::Float64, delta::Float64, resetPeriod, clusterVector::ClusterVector, waitTime::Int64, io::Tuple{IO, Channel})
+	refCNA = Vector{Pair{Tuple{UInt8, UInt8, UInt8}, UInt16}}()
+	refID = -1
+	timeElapsed = 0
+	hist = zeros(Int64, trunc(Int64, 1/delta))
+	return HISTOMetC(kT, w, delta, refCNA, refID, clusterVector, waitTime, hist, io)
+end
+
+function getAcceptanceBoolean(MetC::HISTOMetC, oldCluster::Cluster, newCluster::Cluster)
+
+	if newCluster.energy < oldCluster.energy
+		return true
+	end
+
+
+	updateHist = false
+	binNew = nothing
+	if length(MetC.refCNA) == 0
+		if MetC.waitTime >= MetC.timeElapsed
+			MetC.refCNA = clusterVector.vec[0].CNA
+			MetC.refID = clusterVector.vec[0].ID
+		end
+		MetC.timeElapsed += 1
+
+		hScore = 0
+	else
+		histSum = sum(hist) # used to normalise bar heights
+		if histSum == 0 # avoid division by zero
+			histSum = 1
+		end
+
+		simOld = getCNASimilarity(getCNA(oldCluster), refCNA)
+		binOld = simOld != 1.0 ? trunc(Int64, 1/delta) : trunc(Int64, simOld/delta) # get bin of old Cluster
+		hOld   = hist[binOld]/histSum # get height (normalised) of bars
+		
+		simNew = getCNASimilarity(getCNA(newCluster), refCNA)
+		binNew = simNew != 1.0 ? trunc(Int64, 1/delta) : trunc(Int64, simNew/delta) # get bin of new Cluster
+		hNew   = hist[binNew]/histSum # get height (normalised) of bars
+
+		hScore = MetC.w * (hOld - hNew)
+		updateHist = true
+	end
+	
+	probability = exp((oldCluster.energy - newCluster.energy + hScore) / MetC.kT)	
+
+	# push a string to the channel then take the string in the channel and print it to the output file.
+	put!(MetC.io[2], "\nChance to accept = " * string(probability))
+	while isready(MetC.io[2])
+		print(MetC.io[1], take!(MetC.io[2]))
+	end
+	
+	accept = probability > rand()
+
+	if accept && updateHist
+		hist[binNew] += 1
+	end
+
+	return accept
+
+end
