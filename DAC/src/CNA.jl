26deba950cd152b05cf2aea4e08f53efe510d035
Agandaur1 <okaresmithn@gmail.com>
2023-10-03 13:05:22 +1300

    fixed error in getCNASimilarity where order of args mattered

--- DAC/src/CNA.jl
+++ DAC/src/CNA.jl
@@ -522,6 +522,13 @@ function getCNASimilarity(x::CNAProfile, y::CNAProfile)
 	Nx = length(x)
 	Ny = length(y)
 
+	if Nx > Ny
+		temp = x
+		x = y
+		y = temp
+		Nx = length(x)
+		Ny = length(y)
+	end
 
 	for i in 1:Nx
 		sig = x[i].first
