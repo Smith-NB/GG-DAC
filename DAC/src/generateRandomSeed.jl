bc24bc6b0a8b6efe4927979e6a3bd59f0f091c2f
Agandaur1 <okaresmithn@gmail.com>
2023-12-11 14:33:45 +1300

    added GCDO

--- DAC/src/generateRandomSeed.jl
+++ DAC/src/generateRandomSeed.jl
@@ -295,11 +295,11 @@ function geometricCentreDisplacement!(atoms::Cluster, alphaMin::Float64, alphaMa
 	azimuth = rand(N) * π/180	# φ
 	
 	for i in 1:N
-		println(atom.positions[i, :])
+		println(atoms.positions[i, :])
 		atoms.positions[i, 1] += rho[i] * sin(polar[i]) * cos(azimuth[i])
 		atoms.positions[i, 2] += rho[i] * sin(polar[i]) * sin(azimuth[i])
 		atoms.positions[i, 3] += rho[i] * cos(polar[i])
-		println(atom.positions[i, :])
+		println(atoms.positions[i, :])
 		println()
 	end
 
